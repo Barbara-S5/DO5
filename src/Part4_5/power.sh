2e12a22d56d82dd5c4594ccee2fd069676920fbb
Barbara-S5 <vsankova398@gmail.com>
2025-05-27 18:36:26 +0700

    final

--- src/Part4_5/power.sh
+++ src/Part4_5/power.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-#export DOCKER_CONTENT_TRUST=1
-docker build . -t app:test
-docker run -d -p 80:81  -v ./nginx/nginx.conf:/etc/nginx/nginx.conf app:test
+docker build -t app:test .
+docker run -d -p 80:81 -v $(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf app:test
